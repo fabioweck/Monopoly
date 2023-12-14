ac7c16a6714934a485b38ddd717160b73330a70b
Fabio Weck <fabio.weck@gmail.com>
2023-12-13 23:26:08 -0700

    minor bugs fixed

--- View/DieView.xaml.cs
+++ View/DieView.xaml.cs
@@ -87,6 +87,9 @@ namespace Monopoly.View
 
         private void btnAuto_Click(object sender, RoutedEventArgs e)
         {
+            btnAuto.Visibility = Visibility.Hidden;
+            btnManual.Visibility = Visibility.Hidden;
+            btnMove.Visibility = Visibility.Hidden;
             GetNumbers();
         }
 
@@ -98,6 +101,7 @@ namespace Monopoly.View
             btnMove.Visibility = Visibility.Visible;
             btnMove.Content = $"Move {PlayerName}";
             txtNumberOfPlaces.Visibility = Visibility.Visible;
+            txtNumberOfPlaces.Focus();
         }
 
         private void btnMove_Click(object sender, RoutedEventArgs e)
